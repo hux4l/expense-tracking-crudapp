d9a493e981d493b25b552b6c83c4c4e183e8a4b4
tobas <tomas.baska@outlook.com>
2021-12-14 18:48:47 +0100

    working sending new expenses

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const value = document.querySelector("#value");
 const category = document.querySelector("#category");
 const date = document.querySelector("#date");
 const btnAdd = document.querySelector("#add");
+const btnFetch = document.querySelector("#fetch");
 const warnings = document.querySelectorAll(".required-message");
 const expenseList = document.querySelector(".list");
 const expenses = [];
@@ -12,7 +13,7 @@ let expense = {};
 
 // just test to create category
 const newCategory = document.createElement("option");
-newCategory.setAttribute("value", "hobby");
+newCategory.setAttribute("value", 4);
 newCategory.innerText = "koníčky";
 category.appendChild(newCategory);
 
@@ -42,13 +43,22 @@ function validateInput(value, category, date) {
 // event handler for add button
 btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
+  // validate inputs
   if (validateInput(value.value, category.value, date.value)) {
-    expenses.push({
-      value: parseFloat(value.value),
-      category: category.value,
+    // create new object
+    expense = {
       date: date.value,
-    });
-    console.log(expenses);
+      value: parseFloat(value.value),
+      category_id: parseInt(category.value),
+    };
+
+    // sends data and store returning promise
+    const data = sendData(expense);
+
+    // gets returned message
+    data
+      .then((response) => response.json())
+      .then((message) => console.log(message.message));
   } else {
     return;
   }
@@ -57,7 +67,26 @@ btnAdd.addEventListener("click", (e) => {
 // async function to get data from API
 async function getData() {
   const data = await fetch("http://localhost/10/api/post/read.php");
+  // return data in json format
   return data.json();
+}
+
+// http headers
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+// send data to api
+async function sendData(obj) {
+  return fetch("http://localhost/10/api/post/create.php", {
+    // method
+    method: "POST",
+    //headers
+    headers: myHeaders,
+    // body of request
+    body: JSON.stringify(obj),
+    // redirect
+    redirect: "follow",
+  });
 }
 
 // get color based on expense value
@@ -65,14 +94,20 @@ const outOrInc = (value) => {
   return +value > 0 ? "income" : "outcome";
 };
 
-// call getting data and render to page
-getData().then((response) => {
-  for (let expense of response.data) {
-    const newExpense = document.createElement("li");
-    newExpense.classList.add(`list-expense`, `${outOrInc(expense.value)}`);
-    newExpense.innerText = `${expense.id} - ${expense.category_name} : ${
-      expense.value
-    } (${new Date(Date.parse(expense.date)).toLocaleDateString()})`;
-    expenseList.appendChild(newExpense);
-  }
+// listener for button to fetch data
+btnFetch.addEventListener("click", (e) => {
+  e.preventDefault();
+  // call getting data and render to page
+  getData().then((response) => {
+    for (let expense of response.data) {
+      // creates new li element, sets classes, based on value green or red
+      const newExpense = document.createElement("li");
+      newExpense.classList.add(`list-expense`, `${outOrInc(expense.value)}`);
+      newExpense.innerText = `${expense.id} - ${expense.category_name} : ${
+        expense.value
+      } (${new Date(Date.parse(expense.date)).toLocaleDateString()})`;
+      // appends li to list on webpage
+      expenseList.appendChild(newExpense);
+    }
+  });
 });
